1ffbd2df390ed22dff82d5291e5123571741ea89
john <kom.huy@gmail.com>
2014-12-05 17:16:01 +0700

    create functionality for get API

--- includes/classes/autoload/RestApiImageSlider.php
+++ includes/classes/autoload/RestApiImageSlider.php
@@ -2,17 +2,78 @@
 class RestApiImageSlider extends RestApi{
 	
 	public function get($params = array()){
+		$image = new oscImageCollection();
+		$image->setDbRes(RestApiResources::getDbRes());
+		
+
+		///////////////////////
+		/// show multi data ///
+		///////////////////////
+		
+		$image_array = array();
+		
+		$image->populate();
+		foreach ($image->getElements() as $element ){
+			$element->load();
+			$image_array[] = array(
+				'id' => $element->getId(),
+				'title' => $element->getTitle(),
+				'image' => $element->getImage(),
+				'link' => $element->getLink()
+			);
+		}
+		
 		return array(
-			'data'=> 'test'			
+			'data' => $image_array
 		);
+		
+		/////////////////////////////
+		// show with filter option // 
+		/////////////////////////////
+		
+		$image->setFilter('id' , 1);
+		
+		if( $image->getTotalCount() > 0 ){
+			$image->populate();
+			$element = $image->getFirstElement();
+		}else {
+			throw new Exception(
+				"404: image Not Found",
+				404
+			);
+		}
+		
+		$element->load();
+		return array(
+			'data' => array(
+				'id' => $element->getId(),
+				'title' => $element->getTitle(),
+				'image' => $element->getImage(),
+				'link' => $element->getLink()
+			)
+		);
+		
+		///////////////////
+		// end of sample //
+		///////////////////
 	}	
 	
 	public function post( $params = array() ){
 		
-		return var_dump($params);
+		$image = new oscImage();
+		$image->setDbRes(RestApiResources::getDbRes());
 		
-// 		return ;
-		
+		$image->setImage($params['POST']['image']);
+		$image->setTitle($params['POST']['title']);
+		$image->setLink($params['POST']['link']);
+	
+		$image->save();
+	
+		return array(
+			'data' => array (
+				'image_id' => $image->getId()
+			)
+		);
 		
 	}
 	
